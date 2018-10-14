df86636f9ac4105005b948e12a47bd37c9701b1c
Santosh <santosh.poudyal@gmail.com>
2018-10-14 11:18:06 +1100

    created controller through g controller and named CURD methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+  get 'movies/show'
+  get 'movies/create'
+  get 'movies/new'
+  get 'movies/edit'
+  get 'movies/update'
+  get 'movies/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

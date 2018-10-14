e45702fe1320ed68ac80e9c294f319673537525c
Santosh <santosh.poudyal@gmail.com>
2018-10-14 12:01:36 +1100

    re written routes to make it prettier, as per rails 5.1.2 version

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-  get 'movies/show'
-  get 'movies/create'
-  get 'movies/new'
-  get 'movies/edit'
-  get 'movies/update'
-  get 'movies/destroy'
+  # get 'movies/index'
+  # get 'movies/show/:id'
+  # post 'movies/create'
+  # get 'movies/new'
+  # get 'movies/edit'
+  # put 'movies/update'
+  # patch 'movies/update'
+  # delete 'movies/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "movies", to:"movies#index"
+  post "movies", to:"movies#create"
+  get "movies/new", to:"movies#new"
+  get "movies/:id", to:"movies#show"
+  get "movies/:id/edit", to:"movies#edit"
+  put "movies/:id", to:"movies#update"
+  patch "movies/:id", to:"movies#update"
+  destroy "movies/:id", to:"movies#destroy" 
+
 end
